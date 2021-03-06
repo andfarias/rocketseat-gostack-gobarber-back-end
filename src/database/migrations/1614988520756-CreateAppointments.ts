2a3de23ac08a81bcb52e4bd7a56e263a66c498a3
Anderson Farias <andersonsantofarias@outlook.com>
2021-03-05 22:00:44 -0300

    Repositório do TypeORM

--- src/database/migrations/1614988520756-CreateAppointments.ts
+++ src/database/migrations/1614988520756-CreateAppointments.ts
@@ -12,6 +12,7 @@ export class CreateAppointments1614988520756
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
