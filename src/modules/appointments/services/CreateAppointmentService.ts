172b0373fefcf8315cdf575029717b4aa032cdaa
Anderson Farias <andersonsantofarias@outlook.com>
2021-04-03 13:51:09 -0300

    Feito os seguintes ajustes: * Reescrevendo Repositórios * Dependency Inversion Principle * Refatorando módulo de usuários * Injeção de dependências * Usando controllers

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
@@ -10,13 +11,19 @@ interface IRequest {
   provider_id: string;
 }
 
+@injectable()
 class CreateAppointmentService {
-  constructor(private appointmentsRepository: IAppointmentsRepository) {}
-  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
 
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate,);
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
 
     if (findAppointmentInSameDate) {
       throw new AppError('This appointment is already booked');
